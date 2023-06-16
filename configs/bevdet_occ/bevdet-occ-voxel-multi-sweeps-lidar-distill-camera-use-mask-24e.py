751a5592e556fc43a9250e7e15cdd21e6336f694
zhanghm1995 <zhanghm_1995@qq.com>
2023-06-16 21:46:49 +0800

    add new configs/bevdet_occ/bevdet-occ-voxel-multi-sweeps-lidar-distill-camera-use-mask-24e.py config

---
+++ configs/bevdet_occ/bevdet-occ-voxel-multi-sweeps-lidar-distill-camera-use-mask-24e.py
@@ -0,0 +1,14 @@
+'''
+Copyright (c) 2023 by Haiming Zhang. All Rights Reserved.
+
+Author: Haiming Zhang
+Date: 2023-06-16 21:41:17
+Email: haimingzhang@link.cuhk.edu.cn
+Description: Use the masked features to compute the distillation losses.
+'''
+
+_base_ = ['./bevdet-lidar-occ-voxel-multi-sweeps-lidar-distill-camera-24e.py']
+
+model = dict(
+    use_distill_mask=True
+)
