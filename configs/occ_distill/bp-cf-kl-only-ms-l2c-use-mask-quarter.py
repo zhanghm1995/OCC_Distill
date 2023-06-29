97dce97aa2f24810be1ba23ee3eac098ae6d0154
zhanghm1995 <zhanghm_1995@qq.com>
2023-06-29 10:01:29 +0800

    add new config for quarter dataset

---
+++ configs/occ_distill/bp-cf-kl-only-ms-l2c-use-mask-quarter.py
@@ -0,0 +1,28 @@
+'''
+Copyright (c) 2023 by Haiming Zhang. All Rights Reserved.
+
+Author: Haiming Zhang
+Date: 2023-06-29 09:56:53
+Email: haimingzhang@link.cuhk.edu.cn
+Description: 
+'''
+
+
+_base_ = ['./lp-cf-ms-l2c-use-mask-quarter.py']
+
+model = dict(
+    freeze_teacher_branch=True,
+    logits_as_prob_feat=True,
+
+    occ_distill_head=dict(
+        type='OccDistillHead',
+        use_kl_loss=True,
+        loss_high_feat=dict(
+            type='L1Loss',
+            loss_weight=0.0),
+        loss_prob_feat=dict(
+            type='KnowledgeDistillationKLDivLoss', 
+            loss_weight=0.1)),
+)
+
+load_from = "bevdet-occ-r50-4d-stereo-24e-student_lidar-occ-teacher-model-quarter.pth"
