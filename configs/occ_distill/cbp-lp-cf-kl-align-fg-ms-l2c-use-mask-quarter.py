6e4593b779760748cf3d5619114e61b895276ea4
zhanghm1995 <zhanghm_1995@qq.com>
2023-07-02 14:50:29 +0800

    add new config

--- configs/occ_distill/cbp-lp-cf-kl-align-fg-ms-l2c-use-mask-quarter.py
+++ configs/occ_distill/cbp-lp-cf-kl-align-fg-ms-l2c-use-mask-quarter.py
@@ -2,16 +2,15 @@
 Copyright (c) 2023 by Haiming Zhang. All Rights Reserved.
 
 Author: Haiming Zhang
-Date: 2023-06-27 10:35:03
+Date: 2023-07-02 14:48:24
 Email: haimingzhang@link.cuhk.edu.cn
 Description: 
 '''
 
-
 _base_ = ['./lp-cf-ms-l2c-use-mask-quarter.py']
 
 model = dict(
-    freeze_teacher_branch=False,
+    freeze_teacher_branch=True,
     logits_as_prob_feat=True,
 
     occ_distill_head=dict(
@@ -23,9 +22,7 @@ model = dict(
             loss_weight=0.1),
         loss_prob_feat=dict(
             type='KnowledgeDistillationKLDivLoss', 
-            loss_weight=1.0)),
+            loss_weight=0.1)),
 )
 
-optimizer = dict(type='AdamW', lr=2e-4, weight_decay=1e-2)
-
-load_from = "bevdet-occ-r50-4d-stereo-24e-student_lidar-occ-teacher-model-quarter.pth"
+load_from = "bevdet-r50-4d-stereo-as-student-lidar-voxel-w-aug-as-teacher-quarter.pth"
