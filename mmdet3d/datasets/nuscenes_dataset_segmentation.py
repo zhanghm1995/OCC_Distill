b8fbccec3e33e76925e6c822497ddd95fc30d68b
zhanghm1995 <zhanghm_1995@qq.com>
2023-07-31 23:42:35 +0800

    [Feature] add the NuScenesDatasetOccpancySegmentation class

---
+++ mmdet3d/datasets/nuscenes_dataset_segmentation.py
@@ -0,0 +1,85 @@
+'''
+Copyright (c) 2023 by Haiming Zhang. All Rights Reserved.
+
+Author: Haiming Zhang
+Date: 2023-07-31 22:37:47
+Email: haimingzhang@link.cuhk.edu.cn
+Description: 
+'''
+
+# Copyright (c) OpenMMLab. All rights reserved.
+import os
+import mmcv
+import torch
+import cv2
+import numpy as np
+from tqdm import tqdm
+
+from .builder import DATASETS
+from .nuscenes_dataset import NuScenesDataset
+from .occ_metrics import Metric_Segmentation_mIoU
+
+colors_map = np.array(
+    [
+        [0,   0,   0, 255],  # 0 undefined
+        [255, 158, 0, 255],  # 1 car  orange
+        [0, 0, 230, 255],    # 2 pedestrian  Blue
+        [47, 79, 79, 255],   # 3 sign  Darkslategrey
+        [220, 20, 60, 255],  # 4 CYCLIST  Crimson
+        [255, 69, 0, 255],   # 5 traiffic_light  Orangered
+        [255, 140, 0, 255],  # 6 pole  Darkorange
+        [233, 150, 70, 255], # 7 construction_cone  Darksalmon
+        [255, 61, 99, 255],  # 8 bycycle  Red
+        [112, 128, 144, 255],# 9 motorcycle  Slategrey
+        [222, 184, 135, 255],# 10 building Burlywood
+        [0, 175, 0, 255],    # 11 vegetation  Green
+        [165, 42, 42, 255],  # 12 trunk  nuTonomy green
+        [0, 207, 191, 255],  # 13 curb, road, lane_marker, other_ground
+        [75, 0, 75, 255], # 14 walkable, sidewalk
+        [255, 0, 0, 255], # 15 unobsrvd
+        [0, 0, 0, 0],  # 16 undefined
+        [0, 0, 0, 0],  # 16 undefined
+    ])
+
+
+
+@DATASETS.register_module()
+class NuScenesDatasetOccpancySegmentation(NuScenesDataset):
+    def get_data_info(self, index):
+        """Get data info according to the given index.
+
+        Args:
+            index (int): Index of the sample data to get.
+
+        Returns:
+            dict: Data information that will be passed to the data
+                preprocessing pipelines. It includes the following keys:
+
+                - sample_idx (str): Sample index.
+                - pts_filename (str): Filename of point clouds.
+                - sweeps (list[dict]): Infos of sweeps.
+                - timestamp (float): Sample timestamp.
+                - img_filename (str, optional): Image filename.
+                - lidar2img (list[np.ndarray], optional): Transformations
+                    from lidar to different cameras.
+                - ann_info (dict): Annotation info.
+        """
+        input_dict = super(NuScenesDatasetOccpancySegmentation, self).get_data_info(index)
+        # standard protocol modified from SECOND.Pytorch
+        input_dict['occ_gt_path'] = self.data_infos[index]['occ_path']
+        if 'lidarseg' in self.data_infos[index].keys():
+            input_dict['lidarseg'] = self.data_infos[index]['lidarseg']
+        return input_dict
+
+    def evaluate(self, occ_results, runner=None, show_dir=None, **eval_kwargs):
+        self.segment_eval_metrics = Metric_Segmentation_mIoU(
+            num_classes=17,
+            use_lidar_mask=False,
+            use_image_mask=True)
+
+        print('\nStarting Evaluation...')
+        for index, occ_pred in enumerate(tqdm(occ_results)):
+            occ_res, lidarseg = occ_pred
+            self.segment_eval_metrics.add_batch(occ_res, lidarseg)
+
+        return self.segment_eval_metrics.count_miou()
